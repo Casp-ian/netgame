95726f79f135116a878d8315ec078a57cb4aff6d
caspian <caspian@koolwaaij.nl>
2025-03-21 00:58:15 +0200

    wip floating

--- src/content/map.rs
+++ src/content/map.rs
@@ -7,17 +7,19 @@ pub fn setup_map(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    // circular base
+    // floor
     commands.spawn((
-        Mesh3d(meshes.add(Circle::new(20.0))),
+        Mesh3d(meshes.add(Cuboid::new(100.0, 1.0, 100.0))),
         MeshMaterial3d(materials.add(Color::WHITE)),
-        Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
+        Transform::from_xyz(0.0, -0.5, 0.0),
         RigidBody::Static,
-        Collider::cuboid(20.0, 20.0, 1.0),
+        Collider::cuboid(100.0, 1.0, 100.0),
     ));
     // cube
     commands.spawn((
         Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
+        RigidBody::Static,
+        Collider::cuboid(1.0, 1.0, 1.0),
         MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
         Transform::from_xyz(0.0, 0.5, 0.0),
     ));
--- src/works/player.rs
+++ src/works/player.rs
@@ -12,37 +12,61 @@ pub struct Head;
 pub fn move_player(
     time: Res<Time>,
     keys: Res<ButtonInput<KeyCode>>,
-    mut qp: Query<(&mut LinearVelocity, &mut AngularVelocity), (With<Player>, Without<Head>)>,
+    mut qp: Query<(&mut LinearVelocity, &ShapeHits), (With<Player>, Without<Head>)>,
     qc: Query<&Transform, (With<Head>, Without<Player>)>,
 ) {
-    let mut thing: Vec3 = Vec3::ZERO;
+    let mut whish: Vec3 = Vec3::ZERO;
+    let mut jump = false;
 
     if keys.pressed(KeyCode::KeyW) {
-        thing -= Vec3::Z;
+        whish -= Vec3::Z;
     }
     if keys.pressed(KeyCode::KeyA) {
-        thing -= Vec3::X;
+        whish -= Vec3::X;
     }
     if keys.pressed(KeyCode::KeyS) {
-        thing += Vec3::Z;
+        whish += Vec3::Z;
     }
     if keys.pressed(KeyCode::KeyD) {
-        thing += Vec3::X;
+        whish += Vec3::X;
     }
 
-    // thing *= time.delta_secs();
-    //
-    let (mut linear, mut angular) = qp.single_mut();
+    if keys.pressed(KeyCode::Space) {
+        jump = true;
+    }
 
-    thing = qc.single().rotation.mul_vec3(thing);
+    let (mut linear, hits) = qp.single_mut();
+    let rotation = qc.single().rotation;
 
-    linear.x = thing.x;
-    linear.y = thing.y;
-    linear.z = thing.z;
+    let float = 20.0;
 
-    angular.x = 0.0;
-    angular.y = 0.0;
-    angular.z = 0.0;
+    for hit in hits.iter() {
+        println!("{:?}", hit);
+        linear.y += float * time.delta_secs();
+    }
+
+    whish = rotation.mul_vec3(whish);
+    whish.y = 0.0;
+
+    whish = whish.normalize_or_zero();
+
+    let speed = 1.0;
+    let gravity = -9.8;
+    let jump_height = 10.0;
+    let max_fall = gravity * 2.0;
+
+    linear.x += whish.x * speed;
+    linear.z += whish.z * speed;
+
+    if jump {
+        linear.y = jump_height;
+    }
+
+    linear.y += gravity * time.delta_secs();
+
+    if linear.y < max_fall {
+        linear.y = max_fall;
+    }
 }
 
 pub fn move_camera(mut motion: EventReader<MouseMotion>, mut q: Query<&mut Transform, With<Head>>) {
@@ -69,20 +93,35 @@ pub fn spawn_player(
 ) {
     let player = (
         Player,
-        Mesh3d(meshes.add(Cuboid::new(0.5, 0.5, 0.5))),
+        Mesh3d(meshes.add(Sphere::new(0.25))),
         MeshMaterial3d(materials.add(Color::srgb_u8(224, 144, 255))),
         Transform::from_xyz(-2.5, 4.5, 9.0),
         RigidBody::Dynamic,
-        Collider::cuboid(0.5, 0.5, 0.5),
+        Collider::sphere(0.25),
+        LockedAxes::new()
+            .lock_rotation_x()
+            .lock_rotation_y()
+            .lock_rotation_z(),
         Visibility::Visible,
+        ShapeCaster::new(
+            Collider::sphere(0.25),    // Shape
+            Vec3::new(0.0, -0.5, 0.0), // Origin
+            Quat::default(),           // Shape rotation
+            Dir3::X,                   // Direction
+        ),
     );
 
     let head = (
         Head,
         Transform::from_xyz(0.0, 0.5, 0.0).looking_at(Vec3::ZERO, Vec3::Y),
+        Visibility::Visible,
     );
 
-    let camera = (Camera3d::default(), Transform::from_xyz(0.0, 0.0, 2.0));
+    let camera = (
+        Camera3d::default(),
+        Transform::from_xyz(0.0, 1.0, 3.0).looking_at(Vec3::ZERO, Vec3::Y),
+        Visibility::Visible,
+    );
 
     commands.spawn(player).with_children(|parent| {
         parent.spawn(head).with_children(|parent| {
