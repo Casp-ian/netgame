024a184deea673885a9983428867d842254ab62e
caspian <caspian@koolwaaij.nl>
2025-03-21 18:34:15 +0200

    floating char controller

--- src/works/player.rs
+++ src/works/player.rs
@@ -1,18 +1,26 @@
 use avian3d::prelude::*;
 use bevy::{input::mouse::MouseMotion, prelude::*};
 
+#[derive(PartialEq)]
+pub enum PlayerState {
+    Grounded,
+    Aerial,
+}
+
 #[derive(Component)]
 #[require(Transform)]
-pub struct Player;
+pub struct Player {
+    state: PlayerState,
+}
 
 #[derive(Component)]
 #[require(Transform)]
 pub struct Head;
 
 pub fn move_player(
-    time: Res<Time>,
+    // time: Res<Time>,
     keys: Res<ButtonInput<KeyCode>>,
-    mut qp: Query<(&mut LinearVelocity, &ShapeHits), (With<Player>, Without<Head>)>,
+    mut qp: Query<(&mut LinearVelocity, &ShapeHits, &mut Player), Without<Head>>,
     qc: Query<&Transform, (With<Head>, Without<Player>)>,
 ) {
     let mut whish: Vec3 = Vec3::ZERO;
@@ -35,14 +43,28 @@ pub fn move_player(
         jump = true;
     }
 
-    let (mut linear, hits) = qp.single_mut();
+    let (mut linear, hits, mut player) = qp.single_mut();
     let rotation = qc.single().rotation;
 
-    let float = 20.0;
+    let grounded = 0.5;
+    let grounded_pad = 0.6;
+    let adjustment = 2.5;
+    let dampening = 0.8;
 
-    for hit in hits.iter() {
-        println!("{:?}", hit);
-        linear.y += float * time.delta_secs();
+    let mut height = 100.0;
+    if let Some(x) = hits.iter().next() {
+        height = x.distance;
+    }
+
+    // grounded state
+    if height > grounded - grounded_pad && height < grounded + grounded_pad {
+        player.state = PlayerState::Grounded;
+        let diff = (height - grounded) / -grounded_pad;
+
+        linear.y += diff * adjustment;
+        linear.y *= dampening; // TODO this should be delta timed
+    } else {
+        player.state = PlayerState::Aerial;
     }
 
     whish = rotation.mul_vec3(whish);
@@ -58,11 +80,11 @@ pub fn move_player(
     linear.x += whish.x * speed;
     linear.z += whish.z * speed;
 
-    if jump {
+    if jump && player.state == PlayerState::Grounded {
         linear.y = jump_height;
     }
 
-    linear.y += gravity * time.delta_secs();
+    // linear.y += gravity * time.delta_secs();
 
     if linear.y < max_fall {
         linear.y = max_fall;
@@ -92,23 +114,31 @@ pub fn spawn_player(
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     let player = (
-        Player,
-        Mesh3d(meshes.add(Sphere::new(0.25))),
+        Player {
+            state: PlayerState::Grounded,
+        },
+        Mesh3d(meshes.add(Capsule3d::new(0.25, 0.1))),
         MeshMaterial3d(materials.add(Color::srgb_u8(224, 144, 255))),
         Transform::from_xyz(-2.5, 4.5, 9.0),
         RigidBody::Dynamic,
-        Collider::sphere(0.25),
+        // GravityScale(0.0),
+        Friction::new(0.0),
+        Collider::capsule(0.25, 0.1),
         LockedAxes::new()
             .lock_rotation_x()
             .lock_rotation_y()
             .lock_rotation_z(),
         Visibility::Visible,
         ShapeCaster::new(
-            Collider::sphere(0.25),    // Shape
-            Vec3::new(0.0, -0.5, 0.0), // Origin
-            Quat::default(),           // Shape rotation
-            Dir3::X,                   // Direction
-        ),
+            Collider::sphere(0.2), // Shape
+            Vec3::ZERO,            // Origin
+            Quat::default(),       // Shape rotation
+            Dir3::X,               // Direction
+        )
+        .with_max_hits(1)
+        .with_ignore_origin_penetration(true)
+        .with_max_distance(100.0)
+        .with_direction(Dir3::NEG_Y),
     );
 
     let head = (
